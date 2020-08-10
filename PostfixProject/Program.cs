ebe8f16d422e02558b2541a9441c3ccd0a39e344
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-10 21:31:12 +0530

    Add project files.

---
+++ PostfixProject/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace PostfixProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String infix;
+            Console.Write("Enter an expression :");
+            infix = Console.ReadLine();
+            String postFix = infixToPostFix(infix);
+            Console.WriteLine("postfix expression is : " + postFix);
+            Console.WriteLine("Evaluation of post fix expression is :" + EvaluatePostFixExpression(postFix));
+        }
+        public static String infixToPostFix(String infix)
+        {
+            StackChar st = new StackChar();
+            char next, symbol;
+            String postFix = "";
+            for (int i = 0; i < infix.Length; i++)
+            {
+                symbol = infix[i];
+                if (symbol == ' ' || symbol == '\t')
+                {
+                    continue;
+                }
+                switch (symbol)
+                {
+                    case '(':
+                        st.push(symbol);
+                        break;
+                    case ')':
+                        while ((next = st.Pop()) != '(')
+                            postFix = postFix + next;
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '^':
+                    case '/':
+                    case '%':
+                        while (!st.IsEmpty() && Precedence(st.Peek()) >= Precedence(symbol))
+                            postFix = postFix + st.Pop();
+                        st.push(symbol);
+                        break;
+                    default:
+                        postFix = postFix + infix[i];
+                        break;
+                }
+            }
+            while (!st.IsEmpty())
+            {
+                postFix = postFix + st.Pop();
+            }
+
+            return postFix; ;
+        }
+        public static int Precedence(char symbol)
+        {
+            switch (symbol)
+            {
+                case '(':
+                    return 0;
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                case '%':
+                    return 2;
+                case '^':
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+        public static int EvaluatePostFixExpression(String postFix)
+        {
+            int x, y;
+            StackInt st = new StackInt();
+
+            for (int i = 0; i < postFix.Length; i++)
+            {
+                if (char.IsDigit(postFix[i]))
+                    st.push(Convert.ToInt32(char.GetNumericValue(postFix[i])));
+                else
+                {
+                    x = st.Pop();
+                    y = st.Pop();
+                    switch (postFix[i])
+                    {
+                        case '+':
+                            st.push(y + x);
+                            break;
+                        case '-':
+                            st.push(y - x);
+                            break;
+                        case '*':
+                            st.push(y * x);
+                            break;
+                        case '/':
+                            st.push(y / x);
+                            break;
+                        case '%':
+                            st.push(y % x);
+                            break;
+                        case '^':
+                            st.push(power(y, x));
+                            break;
+
+                    }
+                }
+            }
+            return st.Pop();
+        }
+        public static int power(int b, int a)
+        {
+            int x = 1;
+            for (int i = 1; i <= a; i++)
+                x = x * b;
+            return x;
+        }
+        
+       
+    }
+}
---
+++ PostfixProject/StackChar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostfixProject
+{
+    class StackChar
+    {
+        private char[] stackArray;
+        int top;
+
+        public StackChar()
+        {
+            stackArray = new char[10];
+            top = -1;
+        }
+        public StackChar(int maxSize)
+        {
+            stackArray = new char[maxSize];
+            top = -1;
+        }
+        public int Size()
+        {
+            return top + 1;
+        }
+        public bool IsEmpty()
+        {
+            return (top == -1);
+        }
+        public bool IsFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+        public void push(char x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is overflow ");
+                return;
+            }
+            top = top + 1;
+            stackArray[top] = x;
+        }
+        public char Pop()
+        {
+            char x;
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack underflow ");
+                throw new System.InvalidOperationException();
+            }
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        }
+        public char Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack underflow ");
+                throw new System.InvalidOperationException();
+            }
+            return stackArray[top];
+        }
+    }
+}
